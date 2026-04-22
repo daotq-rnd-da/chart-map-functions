998305b7ad8fd5ba73ce2cd52470bdaa3f00a4ef
daotq.rnd <30324001@student.vgu.edu.vn>
2026-04-22 11:18:42 +0700

    Add function to include scale bar in leaflet maps
    This function enhances leaflet maps by adding a customizable scale bar for better user experience.

---
+++ r_functions/AddScaleBarForMap.R
@@ -0,0 +1,30 @@
+#' Add a scale bar to a leaflet map
+#'
+#' This function adds a scale bar to a leaflet map object,
+#' making maps more informative and user-friendly.
+#'
+#' @param map A leaflet map object to which the scale bar will be added.
+#' @param position Character string specifying the position of the scale bar
+#'   on the map. Options include "bottomleft", "bottomright", "topleft",
+#'   and "topright". Default is "bottomleft".
+#'
+#' @return A leaflet map object with the scale bar added.
+#' @examples
+#' AddScaleBarForMap(map)
+#' AddScaleBarForMap(map, position = "topright")
+
+AddScaleBarForMap <- function(map, 
+                              position = "bottomleft") {
+  map <- map %>%
+    addScaleBar(
+      position = position ,
+      options = scaleBarOptions(
+        maxWidth = 100,
+        # in pixels
+        metric = TRUE,
+        imperial = FALSE,
+        updateWhenIdle = TRUE
+      )
+    )
+  return(map)
+}
