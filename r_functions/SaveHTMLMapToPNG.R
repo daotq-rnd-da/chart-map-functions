587e2aec381fabfdad1b79d199ec44a560282327
Dao Tran <30324001@student.vgu.edu.vn>
2026-04-26 12:54:14 +0700

    Enhance SaveHTMLMapToPNG function and documentation
    Updated function documentation and parameters for clarity. Added folder creation for saving PNG files.

--- r_functions/SaveHTMLMapToPNG.R
+++ r_functions/SaveHTMLMapToPNG.R
@@ -1,34 +1,44 @@
-#' Save a leaflet map as PNG
+#' @title Save a leaflet html map as PNG
 #'
 #' @desciption This function saves a leaflet map object as a PNG image.
 #' The output image can be customized in terms of resolution and control visibility.
-#' 
 #' @author daotq  
-#' @param map A leaflet map object to be saved as PNG.
-#' @param file_name Character string specifying the output file name. Default is "my_map.png".
-#' @param width Numeric value for the capture width (in pixels). Default is 1200.
-#' @param height Numeric value for the capture height (in pixels). Default is 900.
+#' 
+#' @param map The map object (Leaflet/Mapview).
+#' @param folder_name Character. The subfolder in your project to save the file. Defaults to "outputs".
+#' @param file_name Character. The name of the output PNG file. Defaults to "my_map.png".
+#' @param width Numeric. The width of the captured image. Defaults to 1200.
+#' @param height Numeric. The height of the captured image. Defaults to 900.
 #'
-#' @return A PNG file saved to the specified location.
-#' @examples
-#' SaveHTMLMapToPNG(map)
-#' SaveHTMLMapToPNG(map, file_name = "custom_map.png", width = 1600, height = 1200)
-
+#' @export
 SaveHTMLMapToPNG <- function(map, 
+                             folder_name = "visuals",
                              file_name = "my_map.png", 
                              width = 1200, 
                              height = 900) {
   
-  library(mapview)
-  library(webshot2)
+  # Setup
+  if (!require("pacman")) install.packages("pacman")
+  # mapview and webshot2 are required for mapshot
+  pacman::p_load(mapview, webshot2, here)
   
-  # Save the map as a PNG file
-  # width and height define the capture size (resolution)
-  mapview::mapshot(map, 
-                   file = file_name, 
-                   vwidth = width, 
-                   vheight = height,
-                   remove_controls = c("zoomControl", "layersControl")) # Hide controls for a cleaner image
+  # Define path 
+  ## Ensure the target directory exists
+  if (!dir.exists(here::here(folder_name))) {
+    dir.create(here::here(folder_name), recursive = TRUE)
+  }
   
-  message(paste("Map successfully saved at:", file_name))
+  ## Construct the full path
+  full_path <- here::here(folder_name, file_name)
+  
+  # Export the map
+  mapview::mapshot(
+    map, 
+    file = full_path, 
+    vwidth = width, 
+    vheight = height,
+    remove_controls = c("zoomControl", "layersControl", "homeButton", "drawControl")
+  )
+  
+  message(paste("Map successfully saved at:", full_path))
 }
